a608e9fba9344af5b9742d12c505d1a7db03ef5d
manish882 <officialmanish0711@gmail.com>
2024-07-02 15:04:24 +0530

    dynamic form

--- src/app/Widget/dynamic-forms/dynamic-forms.component.ts
+++ src/app/Widget/dynamic-forms/dynamic-forms.component.ts
@@ -1,20 +1,45 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-forms',
   templateUrl: './dynamic-forms.component.html',
   styleUrls: ['./dynamic-forms.component.scss']
 })
-export class DynamicFormsComponent {
-  dynamicForm!:FormGroup;
-  
-  constructor(){
+export class DynamicFormsComponent implements OnInit {
+  @Input() formConfig: any[] = [];
+  dynamicForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.dynamicForm = this.fb.group({
+      items: this.fb.array([])
+    });
   }
 
-  onSubmit(){
-
+  ngOnInit(): void {
+    this.buildForm();
   }
 
+  get items(): FormArray {
+    return this.dynamicForm.get('items') as FormArray;
+  }
+
+  buildForm(): void {
+    const formArray = this.dynamicForm.get('items') as FormArray;
+    this.formConfig.forEach(field => {
+      const control = new FormControl(
+        '',
+        field.validators ? field.validators : []
+      );
+      formArray.push(control);
+    });
+  }
+
+  onSubmit(): void {
+    if (this.dynamicForm.valid) {
+      console.log('Form Submitted', this.dynamicForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
--- src/app/components/dynamic-form/dynamic-form.component.ts
+++ src/app/components/dynamic-form/dynamic-form.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-form',
   templateUrl: './dynamic-form.component.html',
   styleUrls: ['./dynamic-form.component.scss']
 })
-export class DynamicFormComponent {
+export class DynamicFormComponent implements OnInit{
+  formConfig: any[] = [
+    { type: 'text', label: 'Name', name: 'name', validators: [Validators.required, Validators.minLength(3)] },
+    { type: 'email', label: 'Email', name: 'email', validators: [Validators.required, Validators.email] },
+    { type: 'number', label: 'Age', name: 'age', validators: [Validators.required, Validators.min(1)] }
+  ];
+  constructor(){
+    
+  }
 
+  ngOnInit(): void {
+    
+  }
 }
